d61d05a426bacd3ad05fa1c5e2d8a20b8cd6a64c
Ray Juang <rayver+github@gmail.com>
2024-06-11 12:54:08 -0700

    Updating deploy scripts and removing old readSheet script.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Step 1: Sync data"
-pushd plaqueGen > /dev/null
-python3 readSheet.py > ../src/plaques.json
-popd > /dev/null
+# pushd plaqueGen > /dev/null
+python3 importer/readSheet.py > public/plaques.json
+# popd > /dev/null
 
 echo "Step 2: Build optimized web package."
 npm run build
 
 echo "Step 3: Deploy build to website."
 az storage azcopy blob upload -c "\$web" --account-name plaquetv -s "build/*" --recursive
+#az storage azcopy blob upload -c "\$web" --account-name plaquetvalpha -s "build/*" --recursive
 
 echo "All done. Deployed to: "
 echo "https://plaquetv.z5.web.core.windows.net/"
